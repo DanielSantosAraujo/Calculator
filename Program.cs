9adb99812c7655f80a2f7e737cf0451c742e50ff
Daniel Santos <dsaraujo718@gmail.com>
2022-10-07 16:19:14 -0300

    Criando projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿ 
+ namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+        }
+    }
+}
