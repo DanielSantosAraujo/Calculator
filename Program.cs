8b2104eed7b77677cda104542aa4ac755d401a7b
Daniel Santos <dsaraujo718@gmail.com>
2022-10-07 16:49:19 -0300

    Adicionando o método Multiplicação

--- Program.cs
+++ Program.cs
@@ -51,6 +51,22 @@ namespace Calculator
 
             Console.WriteLine("");
 
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Multiplicacao(){
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1  = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2  = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
             float resultado = v1 * v2;
             Console.WriteLine($"O resultado da subtração é {resultado}");
             Console.ReadKey();
